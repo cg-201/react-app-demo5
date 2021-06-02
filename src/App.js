9e7587329d7f23280fd5d813a41ceb61ec16a09f
santosh <santosh.ece06@gmail.com>
2021-06-02 09:55:10 +0530

    basic of routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { AboutUs } from "./components/AboutUs";
 import { Project } from "./components/Project";
@@ -7,12 +8,27 @@ import { ContactUs } from "./components/ContactUs";
 
 function App() {
   return (
-    <div>
-      <Home />
-      <AboutUs />
-      <Project />
-      <ContactUs />
-    </div>
+    <Router>
+      <Route path="/home">
+        <Home />
+      </Route>
+
+      <Route path="/about-us">
+        <AboutUs />
+      </Route>
+
+      <Route path="/project">
+        <Project />
+      </Route>
+
+      <Route path="/contact-us">
+        <ContactUs />
+      </Route>
+
+      <Route path="/">
+        <Home />
+      </Route>
+    </Router>
   );
 }
 

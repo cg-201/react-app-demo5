5dface246bc966c9d1cec8b32f08e9e9723b968d
santosh <santosh.ece06@gmail.com>
2021-06-02 10:29:22 +0530

    userinteface.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 export function Home() {
   return (
-    <div className="bg-primary" style={{ height: "100vh" }}>
-      <h1 className="text-light">Home Page</h1>
+    <div>
+      {/** CAPTURE INPUT HERE  */}
+      <div className="d-flex mt-2 mx-4">
+        <input
+          className="form-control w-75"
+          type="text"
+          placeholder="tweer here"
+        />
+        <input type="button" className="w-25 btn btn-secondary" value="Tweet" />
+      </div>
+
+      {/** DISPLAY TWEET HERE */}
+      <div className="mx-4 mt-4">
+        <h4 className="text-secondary">Tweet 1</h4>
+      </div>
     </div>
   );
 }
